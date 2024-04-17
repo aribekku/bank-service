47b6377045723136567c04c7e7ecf7af4a599658
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-04-17 17:46:10 +0600

    Abstract class with basic entity parameters was created

---
+++ src/main/java/application/models/BaseEntity.java
@@ -0,0 +1,29 @@
+package application.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime created;
+    private Boolean isDeleted;
+
+    @PrePersist
+    public void setCreate()
+    {
+        this.created = LocalDateTime.now();
+    }
+}
