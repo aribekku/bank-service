5ce45e69de8a57b73a19fa73e30245a6136ccab1
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-04-17 17:49:59 +0600

    Transaction model class with parameters was created

---
+++ src/main/java/application/models/Transaction.java
@@ -0,0 +1,24 @@
+package application.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transactions")
+public class Transaction extends BaseEntity {
+
+    private Long accountFrom;
+
+    private Long accountTo;
+
+    private String currencyShortName;
+
+    private Double sum;
+
+    private String expenseCategory;
+}
