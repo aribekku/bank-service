aa746023d04efda991f26d6a9e93ce6f70927992
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-04-19 00:32:21 +0600

    Data validation beans were added

--- src/main/java/application/DTO/CreateTransactionDTO.java
+++ src/main/java/application/DTO/CreateTransactionDTO.java
@@ -1,5 +1,6 @@
 package application.DTO;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,13 +9,19 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CreateTransactionDTO {
 
+    @NotNull
     private Long accountFrom;
 
+    @NotNull
     private Long accountTo;
 
+    @Size(max = 5, message = "Currency short name must be not more than 5 characters")
     private String currencyShortName;
 
+    @NotEmpty
+    @Min(1)
     private Double sum;
 
+    @NotBlank(message = "Expense category is required")
     private String expenseCategory;
 }
--- src/main/java/application/DTO/SetNewLimitDTO.java
+++ src/main/java/application/DTO/SetNewLimitDTO.java
@@ -1,5 +1,7 @@
 package application.DTO;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +11,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SetNewLimitDTO {
 
+    @Min(value = 1, message = "Limit sum can not be 0 or less")
     private Double limit;
 
+    @NotBlank(message = "Expense category is required")
     private String expenseCategory;
 }
