f82112e6b4019e789ca7b09f44b34445716361fd
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-04-18 16:34:44 +0600

    Repository interface for monthlyLimit entity was created

---
+++ src/main/java/application/repositories/MonthlyLimitRepository.java
@@ -0,0 +1,13 @@
+package application.repositories;
+
+import application.models.MonthlyLimit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MonthlyLimitRepository extends JpaRepository<MonthlyLimit, Long> {
+
+    MonthlyLimit findFirstByExpenseCategoryOrderByCreatedDesc(String category);
+
+    List<MonthlyLimit> findAllByLimitExceeded(Boolean exceeded);
+}
