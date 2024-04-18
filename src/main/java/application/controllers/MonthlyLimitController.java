b2bd8202f1b82ae7fd55a96b87385280fc1127a1
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-04-19 00:34:08 +0600

    Tags for swagger were added

--- src/main/java/application/controllers/MonthlyLimitController.java
+++ src/main/java/application/controllers/MonthlyLimitController.java
@@ -2,11 +2,14 @@ package application.controllers;
 
 import application.DTO.SetNewLimitDTO;
 import application.services.MonthlyLimitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@Tag(name = "Limits API")
 @RequestMapping("/limits")
 public class MonthlyLimitController {
 
@@ -17,17 +20,20 @@ public class MonthlyLimitController {
     }
 
     @PostMapping()
+    @Operation(summary = "Endpoint for setting new limit")
     public ResponseEntity<?> setNewLimit(@RequestBody SetNewLimitDTO newLimitDTO) {
         limitService.setNewLimit(newLimitDTO);
         return ResponseEntity.ok("New limit was successfully created!");
     }
 
     @GetMapping
+    @Operation(summary = "Endpoint for getting all limits from database")
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok(limitService.getAllLimits());
     }
 
     @GetMapping("/set")
+    @Operation(summary = "Endpoint for getting only new limits that were set manually")
     public ResponseEntity<?> getAllSetLimits() {
         return ResponseEntity.ok(limitService.getAllSetLimits());
     }
--- src/main/java/application/controllers/TransactionController.java
+++ src/main/java/application/controllers/TransactionController.java
@@ -1,14 +1,15 @@
 package application.controllers;
 
 import application.DTO.CreateTransactionDTO;
-import application.DTO.GetTransactionDTO;
 import application.services.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
+@Tag(name = "Transactions API")
 @RequestMapping("/transactions")
 public class TransactionController {
 
@@ -19,17 +20,20 @@ public class TransactionController {
     }
 
     @PostMapping
+    @Operation(summary = "Endpoint for saving transaction to database")
     public ResponseEntity<?> createTransaction(@RequestBody CreateTransactionDTO transactionDTO) {
         transactionService.save(transactionDTO);
         return ResponseEntity.ok("Transaction was successfully created!");
     }
 
     @GetMapping
+    @Operation(summary = "Endpoint for getting all transactions")
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok(transactionService.getAll());
     }
 
     @GetMapping("/limit-exceeded")
+    @Operation(summary = "Endpoint for getting transactions that exceeded limit")
     public ResponseEntity<?> getTransactionsExceededLimit() {
         return ResponseEntity.ok(transactionService.getTransactionsExceededLimit());
     }
