7b0cee8a8fd077ef82da91f860dee5e8e277f08e
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-04-18 23:21:15 +0600

    Methods for getting all limits and set limits only were added

--- src/main/java/application/services/MonthlyLimitService.java
+++ src/main/java/application/services/MonthlyLimitService.java
@@ -1,9 +1,14 @@
 package application.services;
 
 import application.DTO.SetNewLimitDTO;
+import application.models.MonthlyLimit;
+
+import java.util.List;
 
 public interface MonthlyLimitService {
 
+    List<MonthlyLimit> getAllLimits();
+    List<MonthlyLimit> getAllSetLimits();
     void setNewLimit(SetNewLimitDTO newLimitDTO);
 
 }
