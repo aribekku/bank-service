1b1a0908d2dc759d4306f544e136585953bb5719
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-04-18 23:25:49 +0600

    Method for getting all transactions was added

--- src/main/java/application/services/TransactionService.java
+++ src/main/java/application/services/TransactionService.java
@@ -2,12 +2,13 @@ package application.services;
 
 import application.DTO.CreateTransactionDTO;
 import application.DTO.GetTransactionDTO;
+import application.models.Transaction;
 
 import java.util.List;
 
 public interface TransactionService {
 
+    List<Transaction> getAll();
     List<GetTransactionDTO> getTransactionsExceededLimit();
-
     void save(CreateTransactionDTO transactionDTO);
 }
