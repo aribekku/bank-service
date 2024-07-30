01bf2244da273c639824216c63177ce814698748
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-07-30 21:51:44 +0500

    Method for finding limit exceeded transactions was added

--- src/main/java/application/repositories/TransactionRepository.java
+++ src/main/java/application/repositories/TransactionRepository.java
@@ -3,5 +3,8 @@ package application.repositories;
 import application.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllByLimitExceeded(boolean exceeded);
 }
