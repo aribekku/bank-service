830ab934e06498192ec279d204669c1a5f724baa
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-07-30 21:54:58 +0500

    Method for getting the latest currency rate was changed

--- src/main/java/application/repositories/CurrencyRateRepository.java
+++ src/main/java/application/repositories/CurrencyRateRepository.java
@@ -4,10 +4,11 @@ import application.models.CurrencyRate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface CurrencyRateRepository extends MongoRepository<CurrencyRate, String> {
 
     Boolean existsByCurrencyAndCreatedAfter(String currency, LocalDateTime date);
 
-    CurrencyRate findByCurrencyAndCreatedAfter(String currency, LocalDateTime date);
+    Optional<CurrencyRate> findTopByCurrencyOrderByCreatedDesc(String currency);
 }
