b3c2cad2e59213a9c62febdbddbabae249b769f5
Alibek Meirkhanov <m.alibek98m@gmail.com>
2024-07-31 16:19:40 +0500

    NoArgsConstructor annotaion was added

--- src/main/java/application/DTO/SetNewLimitDTO.java
+++ src/main/java/application/DTO/SetNewLimitDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -11,6 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SetNewLimitDTO {
 
     @Min(value = 1, message = "Limit sum can not be 0 or less")
